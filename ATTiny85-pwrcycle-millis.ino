c9768f9a85335ec03fbf43bd970e8301ee429c24
Jeremy S Cook <jscook55@gmail.com>
2020-05-04 12:15:23 -0400

    Update ATTiny85-pwrcycle-millis.ino

--- ATTiny85-pwrcycle-millis.ino
+++ ATTiny85-pwrcycle-millis.ino
@@ -1,8 +1,8 @@
 //Raspberry Pi power down code
 //Used with ATtiny85 to preserve power when only intermittent operation is needed
 
-unsigned long currentMillis = millis();
-unsigned long previousMillis = millis();
+unsigned long currentMillis = 0;
+unsigned long previousMillis = 0;
 unsigned long onInterval = 150000; //on for 2:30 for powerup, 90 second process, and shutdown
 unsigned long offInterval = 10000; //stay off for 10 seconds in testing, modify either delay as needed
 
@@ -13,11 +13,13 @@ void setup() {
 
 void loop() {
   currentMillis = millis();
-  if (currentMillis - previousMillis >= (offInterval + onInterval)) {
+  if (currentMillis - previousMillis < onInterval) {
     digitalWrite(2, HIGH);
-    previousMillis = millis();
   }
-  else if (currentMillis - previousMillis >= onInterval) {
+  if (currentMillis - previousMillis >= onInterval) {
     digitalWrite(2, LOW);
+  }
+  if (currentMillis - previousMillis >= (onInterval + offInterval)) {
+    previousMillis = currentMillis;
   }
 }
